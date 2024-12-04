4b74ca13158c0384dd2cf4c2247be94fe36776e7
emaunz-parkitny <etienne.maunz-parkitny@wakeo.co>
2024-12-04 20:51:31 +0100

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: '*.candylau.fr',
+    origin: 'https://app.candylau.fr',
   });
 
   await app.listen(process.env.PORT ?? 3000);
